acad9de1513cc3721801a4c83edffbe8231eada2
K8672 <K8672@student.jamk.fi>
2017-01-19 14:52:42 +0200

    teht 7 done

--- teht07/Program.cs
+++ teht07/Program.cs
@@ -10,6 +10,26 @@ namespace teht07
     {
         static void Main(string[] args)
         {
+            int vuosi, luku;
+
+            Console.WriteLine("Anna vuosi > ");
+            string line = Console.ReadLine();
+            vuosi = int.Parse(line);
+
+            
+            if (vuosi % 400 == 0 )
+            {
+                Console.WriteLine("Vuosi on karkausvuosi");
+            }
+            else if ( vuosi % 4 == 0 && vuosi % 100 !=0)
+            {
+                Console.WriteLine("Vuosi on karkausvuosi");
+            }
+            else
+            {
+                Console.WriteLine("Vuosi ei ole karkausvuosi");
+            }
+            Console.ReadLine();
         }
     }
 }
