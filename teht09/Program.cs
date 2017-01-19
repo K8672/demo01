9d2bae604285509dc18e3cf4d2c53a1ea1fe8abc
K8672 <K8672@student.jamk.fi>
2017-01-19 15:16:20 +0200

    teht 9 done

---
+++ teht09/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku, summa = 0;
+
+            do {
+                Console.WriteLine("Anna luku > ");
+                string line1 = Console.ReadLine();
+                luku = int.Parse(line1);
+                summa = summa + luku;
+            } while (luku != 0);
+
+            Console.WriteLine("Lukujen summa on " + summa);
+            Console.ReadLine();
+        }
+    }
+}
