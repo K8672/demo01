b1b71bc3efad16864df57640826f927162bc94b0
K8672 <K8672@student.jamk.fi>
2017-01-19 14:00:40 +0200

    teht 6 done

---
+++ teht06/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace teht06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kulutus = 0.0702;
+            double bensa = 1.595;
+            double matka, kuluu, euro;
+
+            Console.WriteLine("Anna matka kilometreinä > ");
+            string line = Console.ReadLine();
+            matka = int.Parse(line);
+
+            kuluu = matka * kulutus;
+            euro = kuluu * bensa;
+
+            Console.WriteLine("Bensaa kuluu " + kuluu + " litraa, kustannus " + euro + " euroa");
+            Console.ReadLine();
+        }
+    }
+}
